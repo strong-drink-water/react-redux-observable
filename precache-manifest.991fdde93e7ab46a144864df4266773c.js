55d956fc38e13c98d88f4ab3e948d20d1bada88e
strong-drink-water <strongdrinkwater@gmail.com>
2021-09-12 21:24:58 +0800

    Updates

---
+++ precache-manifest.991fdde93e7ab46a144864df4266773c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cec4bef2a17d675b83e3a536fcce4e3",
+    "url": "/react-redux-observable/index.html"
+  },
+  {
+    "revision": "27937c99c57198eb324d",
+    "url": "/react-redux-observable/static/css/main.8b72b85b.chunk.css"
+  },
+  {
+    "revision": "d6d6055d061c0664d453",
+    "url": "/react-redux-observable/static/js/2.ff3c9f9d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react-redux-observable/static/js/2.ff3c9f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27937c99c57198eb324d",
+    "url": "/react-redux-observable/static/js/main.7a0a43dd.chunk.js"
+  },
+  {
+    "revision": "5885913b2baa6d3bff7a",
+    "url": "/react-redux-observable/static/js/runtime-main.e7073b55.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e0d36edc85cb498d421465fbc62d8e5.js"
+  "/react-redux-observable/precache-manifest.991fdde93e7ab46a144864df4266773c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-observable/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
